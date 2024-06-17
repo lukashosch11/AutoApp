f3af41681350f376cd0b3c6e4cc1afcc1757a03a
lukas <luky.hosch@gmail.com>
2024-06-17 18:23:12 +0200

    Add, New, Delete

--- AutoWebApp/AutoWebApp/Controllers/AutoController.cs
+++ AutoWebApp/AutoWebApp/Controllers/AutoController.cs
@@ -1,6 +1,7 @@
 using AutoWebApp.Context;
 using AutoWebApp.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoWebApp.Controllers;
 
@@ -15,5 +16,42 @@ public class AutoController : Controller
         List<Auto> auto = dbctx.Auto.ToList();
         //Der View übergeben
         return View(auto);
+    }
+
+    [HttpPost]
+    public IActionResult SaveAuto(Auto auto)
+    {
+        MyDbContext dbctx = new MyDbContext();
+        dbctx.Auto.Add(auto);
+        dbctx.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
+    public IActionResult UpdateAuto(Auto auto)
+    {
+        MyDbContext dbctx = new MyDbContext();
+        dbctx.Auto.Update(auto);
+        dbctx.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    public IActionResult New()
+    {
+        return View();
+    }
+
+    public IActionResult Edit(Guid id)
+    {
+        MyDbContext dbctx = new MyDbContext();
+        Auto auto = dbctx.Auto.FirstOrDefault(a => a.Autoid == id);
+        return View(auto);
+    }
+    
+    public IActionResult Delete(Guid id)
+    {
+        MyDbContext dbctx = new MyDbContext();
+        Auto autoToDelete = dbctx.Auto.FirstOrDefault(a => a.Autoid == id);
+        dbctx.Auto.Remove(autoToDelete);
+        dbctx.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
